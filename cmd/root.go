b130e4de13e8b500c3c4c6b7643e776b3d9b93c2
PiquelChips <63727792+PiquelChips@users.noreply.github.com>
2025-03-13 20:09:47 +0100

    Added root command and loading config

---
+++ cmd/root.go
@@ -0,0 +1,36 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/PiquelChips/piquel-cli/config"
+	"github.com/spf13/cobra"
+)
+
+var (
+	rootCmd = &cobra.Command{
+		Use:   "piquel-cli",
+		Short: "Piquel's CLI",
+        Run: func(cmd *cobra.Command, args []string) {
+            config.LoadConfig(configPath)
+        },
+	}
+	configPath string
+)
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	userHomeDir, err := os.UserHomeDir()
+	if err != nil {
+		panic(err)
+	}
+
+	rootCmd.PersistentFlags().StringVar(&configPath, "config", fmt.Sprintf("%s/.config/piquel.yaml", userHomeDir), "config file (default is $HOME/.config/piquel.yaml)")
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/PiquelChips/piquel-cli/cmd"
+
+func main() {
+	cmd.Execute()
+}
