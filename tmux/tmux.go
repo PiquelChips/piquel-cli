ab3b8cc52d91c3f622795f694071e8ebafb2c09e
PiquelChips <63727792+PiquelChips@users.noreply.github.com>
2025-03-15 11:00:41 +0100

    feat: tmux wrapper

---
+++ tmux/tmux.go
@@ -0,0 +1,83 @@
+package tmux
+
+import (
+	"os"
+	"os/exec"
+	"regexp"
+	"strings"
+
+	"github.com/PiquelChips/piquel-cli/config"
+)
+
+func ListSessions() ([]string, error) {
+	listSessionsCommand := exec.Command("tmux", "list-sessions")
+
+	sessionBytes, err := listSessionsCommand.Output()
+	if err != nil {
+		return []string{}, err
+	}
+
+	regex := regexp.MustCompile("^[^:]+")
+	return regex.FindAllString(string(sessionBytes), -1), nil
+}
+
+func Attach(session string) (string, error) {
+	return execTmuxReturn("attach", "-t", session)
+}
+
+func NewSession(sessionName string, session *config.SessionConfig) error {
+	if err := execTmux("new-session", "-Ad", "-c", session.Root, "-s", sessionName); err != nil {
+		return err
+	}
+
+	index, err := execTmuxReturn("list-windows", "-t", sessionName, "-F", "#{window_index}")
+	if err != nil {
+		return err
+	}
+	index = strings.Trim(index, "\n")
+
+	for _, window := range session.Windows {
+		if err := NewWindow(session.Root, window); err != nil {
+			return err
+		}
+	}
+
+	if err := execTmux("kill-window", "-t", sessionName+":"+index); err != nil {
+		return err
+	}
+
+	if err := execTmux("select-window", "-t", sessionName+":"+index); err != nil {
+		return err
+	}
+
+	_, err = Attach(sessionName)
+	return err
+}
+
+func NewWindow(startDir string, window *config.WindowConfig) error {
+	err := execTmux("new-window", "-c", startDir)
+	if err != nil {
+		return err
+	}
+
+	for _, command := range window.Commands {
+		if err := execTmux("send-keys", command, "Enter"); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func execTmux(args ...string) error {
+	command := exec.Command("tmux", args...)
+	command.Stdin = os.Stdin
+	return command.Run()
+}
+
+func execTmuxReturn(args ...string) (string, error) {
+	command := exec.Command("tmux", args...)
+	command.Stdin = os.Stdin
+	result, err := command.CombinedOutput()
+	return string(result), err
+}
