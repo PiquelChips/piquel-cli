10d996ed8b1f0fb049eb5a4ecf2830b6c9643a05
PiquelChips <63727792+PiquelChips@users.noreply.github.com>
2025-03-15 10:56:38 +0100

    fix: redirect ~ paths to home dir in config loading

--- config/config.go
+++ config/config.go
@@ -1,24 +1,35 @@
 package config
 
 import (
+	"errors"
 	"fmt"
 	"os"
 
+	"github.com/PiquelChips/piquel-cli/utils"
 	"gopkg.in/yaml.v3"
 )
 
 var Config PiquelConfig
 var configLoaded bool = false
 
-func LoadConfig(configPath string) {
-    if configLoaded {
-        panic(fmt.Errorf("Config has already been loaded from %s", configPath))
-    }
+func LoadConfig(configPath string) error {
+	if configLoaded {
+		return fmt.Errorf("Config has already been loaded from %s", configPath)
+	}
 
-    configFile, err := os.ReadFile(configPath)
-    if err != nil {
-        panic(fmt.Errorf("Config file %s does not exist", configPath))
-    }
+	configFile, err := os.ReadFile(configPath)
+	if err != nil {
+		return fmt.Errorf("Config file %s does not exist", configPath)
+	}
 
-    yaml.Unmarshal(configFile, &Config)
+	yaml.Unmarshal(configFile, &Config)
+
+	for name, session := range Config.Sessions {
+		session.Root = utils.ExpandHome(session.Root)
+		if _, err := os.Stat(session.Root); errors.Is(err, os.ErrNotExist) {
+			return fmt.Errorf("Path %s of session %s does not exist: ", session.Root, name)
+		}
+	}
+
+	return nil
 }
--- config/types.go
+++ config/types.go
@@ -1,16 +1,14 @@
 package config
 
 type PiquelConfig struct {
-	Sessions map[string]SessionConfig `yaml:"sessions"`
+	Sessions map[string]*SessionConfig `yaml:"sessions"`
 }
 
 type SessionConfig struct {
-	Root         string                  `yaml:"root"`
-	SelectWindow int                     `yaml:"select_window"`
-	Windows      []WindowConfig `yaml:"windows"`
+	Root         string          `yaml:"root"`
+	Windows      []*WindowConfig `yaml:"windows"`
 }
 
 type WindowConfig struct {
-	Name     string   `yaml:"string"`
 	Commands []string `yaml:"commands"`
 }
