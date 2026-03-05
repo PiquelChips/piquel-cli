5940e2fa1f87e10810ef484474f8f98f2e3216bf
PiquelChips <piquel@piquel.fr>
2026-03-05 10:37:31 +0100

    add base configuration models

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,23 @@
 pub mod cli;
+pub mod tmux;
+
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct WindowConfig {
+    pub commands: Vec<String>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct SessionConfig {
+    pub root: String,
+    pub windows: Vec<WindowConfig>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Config {
+    pub sessions: HashMap<String, SessionConfig>,
+    pub validate_session_root: bool,
+    pub default_session: Vec<WindowConfig>, // a session without a root
+}
