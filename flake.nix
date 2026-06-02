aef9b677166fa03df896d148f6a0ba0a75c60f76
piquel <piquel@piquel.fr>
2026-06-02 18:33:29 +0200

    add nix flake checks

--- flake.nix
+++ flake.nix
@@ -18,6 +18,73 @@
         in
         {
             packages.default = piquelcli;
+            checks =
+                {
+                    package = piquelcli;
+
+                    rust-workflow = pkgs.rustPlatform.buildRustPackage {
+                        pname = "piquelcli-rust-workflow";
+                        version = piquelcli.version;
+                        src = pkgs.lib.cleanSource ./.;
+                        cargoLock.lockFile = ./Cargo.lock;
+                        nativeBuildInputs = with pkgs; [
+                            rustfmt
+                            clippy
+                            fzf
+                            git
+                            tmux
+                        ];
+                        buildPhase = ''
+                            runHook preBuild
+                            cargo fmt --check
+                            cargo clippy --all-targets --all-features -- -D warnings
+                            runHook postBuild
+                        '';
+                        checkPhase = ''
+                            runHook preCheck
+                            cargo test --all-targets --all-features
+                            runHook postCheck
+                        '';
+                        installPhase = ''
+                            runHook preInstall
+                            touch "$out"
+                            runHook postInstall
+                        '';
+                    };
+                } // pkgs.lib.optionalAttrs pkgs.stdenv.isLinux {
+                    nixos-module =
+                        (nixpkgs.lib.nixosSystem {
+                            inherit system;
+                            modules = [
+                                self.nixosModules.default
+                                {
+                                    programs.piquelcli = {
+                                        enable = true;
+                                        settings = {
+                                            default_session = "default";
+                                            sessions.default.windows = [
+                                                { commands = [ "echo ready" ]; }
+                                            ];
+                                            projects = [
+                                                {
+                                                    repository = "https://github.com/PiquelChips/piquel-cli.git";
+                                                    name = "piquel-cli";
+                                                    default_session = "default";
+                                                }
+                                                {
+                                                    repository = "git@github.com:owner/inline.git";
+                                                    name = "inline";
+                                                    default_session.windows = [
+                                                        { commands = [ "cargo check" ]; }
+                                                    ];
+                                                }
+                                            ];
+                                        };
+                                    };
+                                }
+                            ];
+                        }).config.programs.piquelcli.package;
+                };
             devShells.default = import ./nix/shell.nix { inherit outputs system pkgs; };
         }
     );
